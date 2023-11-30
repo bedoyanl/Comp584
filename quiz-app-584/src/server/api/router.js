d79f10c8aad0b48f72b7301bab7ae5106e90c349
bedoyanl <laurabedoyan@gmail.com>
2023-11-29 16:09:54 -0800

    Get response from GPT API and print to Vue UI

--- quiz-app-584/src/server/api/router.js
+++ quiz-app-584/src/server/api/router.js
@@ -1,24 +1,47 @@
-const express = require("express");
+const express = require('express');
+const { OpenAI } = require('openai');
+const axios = require('axios');
 const router = express.Router();
+const bodyParser = require('body-parser');
+require('dotenv').config();
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+// Initializing the OpenAI API client
+const openai = new OpenAI({
+  key: process.env.OPENAI_API_KEY,
+  // Add other configuration options if needed
+});
+
+// Defining a conversation context prompt
+const conversationContextPrompt =
+  " You are a teacher for a class. Please come up with 10 quiz questions for the human based on the information after the colon:";
 
 router.use((req, res, next) => {
-    console.log(`Request received at ${new Date()}`);
-    next();
+  console.log(`Request received at ${new Date()}`);
+  next();
 });
 
-router.get("/", (req, res) => {
-    res.json({
-        message: "Hi from /api/!",
-    });
-    res.send();
-});
+router.post('/chat', async (req, res) => {
+  // Extracting the user's message from the request body
+  const message = req.body.message;
 
-router.post("/", (req, res) => {
-    console.log(req.body);
-    const testData = {
-        Quiz: "What's your favorite letter?\nA)\nB)\nC)\nD)",
-    };
-    res.send(testData);
+  // Calling the OpenAI API to complete the message
+  await openai.completions.create({
+    model: 'text-davinci-003',
+    prompt: conversationContextPrompt + message,
+    max_tokens: 1024,
+    
+  })
+  .then((response) => {
+    // Sending the response data back to the client
+    res.send(response.choices);
+  })
+  .catch((error) => {
+    // Handle errors
+    console.error('OpenAI API Error:', error);
+    res.status(500).send('Internal Server Error');
+  });
 });
 
 module.exports = router;
--- quiz-app-584/src/server/index.js
+++ quiz-app-584/src/server/index.js
@@ -3,21 +3,21 @@ const conf = require("./config/server-config");
 const history = require("connect-history-api-fallback");
 const { resolve } = require("path");
 
-//Instance
+// Instance
 const instance = express();
-const PORT = 8080;
+const { port } = conf; // Use the port from the configuration
 
-//API
+// API
 conf(instance);
 
-//UI
+// UI
 const publicPath = resolve(__dirname, "../../dist");
 const staticConf = { maxAge: "1y", etag: false };
 
 instance.use(express.static(publicPath, staticConf));
 instance.use("/", history());
 
-//Start server
-instance.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+// Start server
+instance.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
