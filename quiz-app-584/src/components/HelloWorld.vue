2add3496e78f3b463dcced2f15e78b3e0f1458af
andrew-miner <andrew.miner3@gmail.com>
2023-12-07 13:03:07 -0800

    Created front-end quiz components.

--- quiz-app-584/src/components/HelloWorld.vue
+++ quiz-app-584/src/components/HelloWorld.vue
@@ -1,26 +1,54 @@
 <template>
   <div>
-    <textarea v-model="userMessage" placeholder="Type your message"></textarea>
-    <button @click="sendMessage">Send Message</button>
-    <div v-if="responseMessages.length > 0">
-      <p v-for="(message, index) in responseMessages" :key="index">{{ message }}</p>
+    <div v-if="responseMessages.length == 0">
+      <textarea v-model="userMessage" placeholder="Type your message"></textarea>
+      <button @click="sendMessage">Send Message</button>
+      <label v-if="errorMessage.length > 0">{{ errorMessage }}</label>
+    </div>
+    <div v-if="responseMessages.length > 0" class="questionWrapper">
+      <span v-if="showInput">
+        <PopupWindow>
+          <p>
+            {{ userMessage }}
+          </p>
+          <button @click="onShowInput" class="buttonClass inputButtonClass">{{inputButtonLabel}}</button>
+        </PopupWindow>
+      </span>
+      <div class="headerDiv">
+        <h2>Your Quiz</h2>
+      </div>
+      <QuizQuestion v-for="(message, index) in responseMessages" :question="message.question" :options="message.options" :answerIndex="message.answerIndex" :key="index" :questionId="index"/> 
+      <button v-if="!showInput" @click="onShowInput" class="buttonClass inputButtonClass">{{inputButtonLabel}}</button>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import QuizQuestion from "./QuizQuestion.vue";
+import PopupWindow from "./PopupWindow.vue";
 
 export default {
   data() {
     return {
       userMessage: '',
       responseMessages: [],
+      errorMessage: '',
+      showInput: false,
     };
   },
+
+  computed: {
+    inputButtonLabel() {
+      if (this.showInput) return "Hide Original Input";
+      else return "Show Original Input"
+    }
+  },
+
   methods: {
     async sendMessage() {
       try {
+        this.errorMessage = '';
         const response = await axios.post('/api/chat', {
           message: this.userMessage,
         });
@@ -28,10 +56,18 @@ export default {
         this.responseMessages = response.data;
       } catch (error) {
         console.error('Error sending message:', error);
-        this.responseMessages = ['An error occurred'];
+        this.errorMessage = 'An error occurred';
       }
     },
+
+    onShowInput() {
+      this.showInput = !this.showInput;
+    },  
   },
+  components: {
+    QuizQuestion,
+    PopupWindow,
+  }
 };
 </script>
 
@@ -49,5 +85,49 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.headerDiv {
+  font-size: 40px;
+  align-self: center;
+  max-width: 600px;
+  width: 100%;
+}
+.question {
+  margin-top: 300px;
+}
+
+.questionWrapper {
+  display: flex; 
+  flex-flow: column nowrap;
+  gap: 60px;
+}
+
+.inputButtonClass {
+  max-width: 600px;
+  width: 100%;
+  height: 30px;
+  font-weight: bold;
+  font-size: 20;
+  align-self: center;
+}
+</style>
+
+<style>
+.buttonClass {
+  border-width:1px;
+  color:#ffffff;
+  border-color:#057fd0;
+  border-top-left-radius:6px;
+  border-top-right-radius:6px;
+  border-bottom-left-radius:6px;
+  border-bottom-right-radius:6px;
+  box-shadow: 0px 1px 0px 0px #f0f7fa;
+  text-shadow: 0px -1px 0px #5b6178;
+  background:linear-gradient(#33bdef, #019ad2);
+}
+
+.buttonClass:hover {
+  background: linear-gradient(#019ad2, #33bdef);
 }
 </style>
