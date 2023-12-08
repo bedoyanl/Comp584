708afc98da163048735f43d87c7b3bb43de50239
andrew-miner <andrew.miner3@gmail.com>
2023-12-07 16:30:19 -0800

    Fixed issues with the back-end server configuration.

--- quiz-app-584/vue.config.js
+++ quiz-app-584/vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
-const conf = require("./src/server/config/server-config");
+const { configure } = require("./src/server/config/server-config");
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
         onBeforeSetupMiddleware: (devServer) => {
-            conf(devServer.app);
+            configure(devServer.app);
         },
     },
 });
